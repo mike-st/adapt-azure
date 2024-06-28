7f7df060fdceb7158ba4733180a66285367d883b
Mike Stevens <mesgraphix@gmail.com>
2024-06-28 15:22:43 -0400

    Added stopping the audio or other videos on play

--- js/adapt-azure.js
+++ js/adapt-azure.js
@@ -168,7 +168,8 @@ define(function(require) {
         trackplayAMP: function() {
             var currentazureon = this.model.get('_id');
             //Trigger PAUSE button from outside the iframe
-            if ( $('.' + currentazureon + ' .removeazureie').hasClass('vjs-playing') ) {
+            if ( $('iframe[name="azuremediaplayer-' + currentazureon + '"]').hasClass('vjs-playing') ) {
+                $('div[data-adapt-id="' + currentazureon + '"] .audio-controls .audio-toggle.audio-pause:not(.audio-play)').trigger("click"); //stop current audio
                 $('iframe:not(#' + currentazureon + ').vjs-playing:not(.vjs-paused)').contents().find(".vjs-play-control.vjs-control.vjs-button.vjs-playing:not(.vjs-paused)").trigger("click");
             }
         },
